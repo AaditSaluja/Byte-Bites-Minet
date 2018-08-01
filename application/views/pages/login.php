0139cfca6629ae7fafae96179096695be86eca31
Aadit Saluja <aaditsaluja@gmail.com>
2018-08-01 23:22:25 +0530

    Tho

---
+++ application/views/pages/login.php
@@ -0,0 +1,7 @@
+<div class="login">
+    <div class="box">
+    <?=form_field(['name'=>'name', 'placeholder'=>'Your Name', 'icon'=>'face', 'required'=>'true', 'maxlength'=>'64', 'minlength'=>'3'])?>
+        <p class="login">LOGIN</p>
+        <img src="">
+    </div>
+</div>
