0d85f907ed872ed364d008a6c9f600df55969b43
Aadit Saluja <aaditsaluja@gmail.com>
2018-08-04 13:32:18 +0530

    Revert "Login + Login Media Queries"
    This reverts commit eb56315c1af2879efecf9c7ce5f39422ded3db51.

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,12 +1,1 @@
-<section class="body">
-<div class="login">
-    <div class="box">
-        <!-- <form>
-            <input type="text">
-            <input type="passw"
-        </form> -->
-        <p class="login">LOGIN</p>
-        <img src="">
-    </div>
-</div>
-</section>
+<?php echo"hello1 " ?>
