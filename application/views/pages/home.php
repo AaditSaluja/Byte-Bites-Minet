eb56315c1af2879efecf9c7ce5f39422ded3db51
Aadit Saluja <aaditsaluja@gmail.com>
2018-08-04 13:28:40 +0530

    Login + Login Media Queries

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,1 +1,12 @@
-<?php echo"hello1 " ?>
+<section class="body">
+<div class="login">
+    <div class="box">
+        <!-- <form>
+            <input type="text">
+            <input type="passw"
+        </form> -->
+        <p class="login">LOGIN</p>
+        <img src="">
+    </div>
+</div>
+</section>
