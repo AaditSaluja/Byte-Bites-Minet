f277975389252799264619aa024dc972630a4747
Aadit Saluja <aaditsaluja@gmail.com>
2018-08-09 02:36:19 +0530

    yo

--- application/views/pages/poll.php
+++ application/views/pages/poll.php
@@ -1,3 +1,189 @@
 <div class="poll-poll">
     Attempt The Poll/Questionnaire
 </div>
+<div><p class="poll-3">Compatibilty Analysis</p>
+<div class="questions">
+
+<h1 class="accordion">Question 1 <i class="icon material-icons">keyboard_arrow_down</i></h1>
+<div class="panel"><p class="p1">Q1 You had to meet your friend at 10:30. Now it is 10:50 and he has not yet arrived, and he is not picking up his phone. What do you do?</p>
+<label class="rad-button r1">
+  <input type="radio" name="radAnswer">
+  <span class="checkmark">Wait for 20 more minutes</span>
+    </label>
+    <br>
+    <label class="rad-button r2">
+  <input type="radio" name="radAnswer">
+  <span class="checkmark">Leave him and proceed</span>
+    </label>
+    <br>
+    <label class="rad-button r3">
+  <input type="radio" name="radAnswer">
+  <span class="checkmark">Wait for 30 more minutes</span>
+ </label> 
+</div>
+<h1 class="accordion">Question 2 <i class="icon material-icons">keyboard_arrow_down</i></h1>
+<div class="panel"><p class="p2">Q2 How do you find introducing yourself to unknown people?</p>
+<label class="rad-button r1">
+  <input type="radio" name="radAnswer2">
+  <span class="checkmark">Difficult</span>
+    </label>
+    <br>
+    <label class="rad-button r2">
+  <input type="radio" name="radAnswer2">
+  <span class="checkmark">Easy</span>
+    </label>
+    <br>
+    <label class="rad-button r3">
+  <input type="radio" name="radAnswer2">
+  <span class="checkmark">Nervy</span>
+ </label>
+</div>
+<h1 class="accordion">Question 3 <i class="icon material-icons">keyboard_arrow_down</i></h1>
+<div class="panel"><p class="p3">Q3 How do you like to maintain your table and desktop screen</p>
+<label class="rad-button r1">
+  <input type="radio" name="radAnswer3">
+  <span class="checkmark">Completely Organized</span>
+    </label>
+    <br>
+    <label class="rad-button r2">
+  <input type="radio" name="radAnswer3">
+  <span class="checkmark">Rough and messed up</span>
+    </label>
+    <br>
+    <label class="rad-button r3">
+  <input type="radio" name="radAnswer3">
+  <span class="checkmark">Just enough organized to find my stuff</span>
+ </label>
+ </div>
+<h1 class="accordion">Question 4 <i class="icon material-icons">keyboard_arrow_down</i></h1>
+<div class="panel"><p class="p4">Q4 Thoughts on Drinking Alcohol during office dinners?</p>
+<label class="rad-button r1">
+  <input type="radio" name="radAnswer4">
+  <span class="checkmark">I don’t support drinking during official events</span>
+    </label>
+    <br>
+    <label class="rad-button r2">
+  <input type="radio" name="radAnswer4">
+  <span class="checkmark">We are all colleagues and drinking together is fine</span>
+    </label>
+    <br>
+    <label class="rad-button r3">
+  <input type="radio" name="radAnswer4">
+  <span class="checkmark">Depends upon whether my colleagues are comfortable with it</span>
+ </label>
+</div>
+<h1 class="accordion">Question 5 <i class="icon material-icons">keyboard_arrow_down</i></h1>
+<div class="panel"><p class="p5">Q5 How do you handle situations where you are way late than the deadline?</p>
+<label class="rad-button r1">
+  <input type="radio" name="radAnswer5">
+  <span class="checkmark">I keep myself motivated and optimistic and keep working</span>
+    </label>
+    <br>
+    <label class="rad-button r2">
+  <input type="radio" name="radAnswer5">
+  <span class="checkmark">I ask for an extension with the customer/my boss</span>
+    </label>
+    <br>
+    <label class="rad-button r3">
+  <input type="radio" name="radAnswer5">
+  <span class="checkmark">I ask for help from employees/ friends</span>
+ </label>
+</div>
+ <h1 class="accordion">Question 6 <i class="icon material-icons">keyboard_arrow_down</i></h1>
+<div class="panel"><p class="p6">Q6 Do you frequently take a nap in the middle of the day?</p>
+<label class="rad-button r1">
+  <input type="radio" name="radAnswer6">
+  <span class="checkmark">Yes</span>
+    </label>
+    <br>
+    <label class="rad-button r2">
+  <input type="radio" name="radAnswer6">
+  <span class="checkmark">No</span>
+    </label>
+    <br>
+    <label class="rad-button r3">
+  <input type="radio" name="radAnswer6">
+  <span class="checkmark">Sometimes</span>
+ </label>
+</div>
+<h1 class="accordion">Question 7 <i class="icon material-icons">keyboard_arrow_down</i></h1>
+<div class="panel"><p class="p7">Q7 Tabs or spaces?</p>
+<label class="rad-button r1">
+  <input type="radio" name="radAnswer7">
+  <span class="checkmark">Tabs</span>
+    </label>
+    <br>
+    <label class="rad-button r2">
+  <input type="radio" name="radAnswer7">
+  <span class="checkmark">Spaces</span>
+    </label>
+    <br>
+    <label class="rad-button r3">
+  <input type="radio" name="radAnswer7">
+  <span class="checkmark">None #NoIndentation</span>
+ </label>
+</div>
+<h1 class="accordion">Question 8 <i class="icon material-icons">keyboard_arrow_down</i></h1>
+<div class="panel"><p class="p8">Q8 Thoughts on meetings after work hours?</p>
+<label class="rad-button r1">
+  <input type="radio" name="radAnswer8">
+  <span class="checkmark">Don’t like it but will attend them</span>
+    </label>
+    <br>
+    <label class="rad-button r2">
+  <input type="radio" name="radAnswer8">
+  <span class="checkmark">Essential</span>
+    </label>
+    <br>
+    <label class="rad-button r3">
+  <input type="radio" name="radAnswer8">
+  <span class="checkmark">I just hate them</span>
+ </label>
+</div>
+<h1 class="accordion">Question 9 <i class="icon material-icons">keyboard_arrow_down</i></h1>
+<div class="panel"><p class="p9">Q9 Preference of timings</p>
+<label class="rad-button r1">
+  <input type="radio" name="radAnswer9">
+  <span class="checkmark">INDIAN</span>
+    </label>
+    <br>
+    <label class="rad-button r2">
+  <input type="radio" name="radAnswer9">
+  <span class="checkmark">EMEA</span>
+    </label>
+    <br>
+    <label class="rad-button r3">
+  <input type="radio" name="radAnswer9">
+  <span class="checkmark">LATAM/ LAC/ NA</span>
+ </label>
+</div>
+</div>
+</div>
+</div>
+
+<?php
+                echo component("button_button", ["icon"=>"send","text"=>"Next Poll", "id"=>"next-poll","class"=>"next-poll"])
+            ?>
+   <script type="text/javascript">
+  var acc = document.getElementsByClassName("accordion");
+  var icons = document.getElementsByClassName("fa");
+  var str = document.getElementsByClassName("fa icon");
+  var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    var icon = this.children;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+      panel.style.display = "none";
+      icon[0].innerHTML = "keyboard_arrow_down";
+    } else {
+      panel.style.maxHeight = 185 + "vh";
+      panel.style.display = "block";
+      icon[0].innerHTML = "keyboard_arrow_up";
+    } 
+  });
+}
+</script>
