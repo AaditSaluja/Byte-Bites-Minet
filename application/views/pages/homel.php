798f99e4a2a87e06251118fe13c3bfac3417981c
Aadit Saluja <aaditsaluja@gmail.com>
2018-08-09 08:30:33 +0530

    Hello World

--- application/views/pages/homel.php
+++ application/views/pages/homel.php
@@ -1,14 +1,21 @@
 <div class="home-cover">
-    <div class="signup-button"> 
 <div class="cover-inner"></div>
-</div>
+
+
+<h1 class="badatext"> Welcome Aboard! </h1>
+<div class="border"></div>
 <div class="home-grid">
-    <a href="/profile"><div class="gr g1">Profile</div></a>
-    <a href="/poll"><div class="gr g2">Poll</div></a>
-    <a href="/suggestions"><div class="gr g3">Suggestions</div></a>
-    <a href="/talk"><div class="gr g4">Talk</div></a>
-    <a href="/upgrade"><div class="gr g5">Upgrade</div></a>
-    <a href="/logout"><div class="gr g6">Logout</div></a>
+    <a href="/profile"><div class="gr g1"><i class="material-icons">account_circle</i><h1>Profile</h1></div></a>
+    <a href="/poll"><div class="gr g2"><i class="material-icons">assignment
+    </i><h1>Poll</h1></div></a>
+    <a href="/suggestions"><div class="gr g3"><i class="material-icons">textsms
+    </i><h1>Talk</h1></div></a>
+    <a href="/talk"><div class="gr g4"><i class="material-icons">insert_emoticon
+    </i><h1>Suggestions</h1></div></a>
+    <a href="/upgrade"><div class="gr g5"><i class="material-icons">card_membership</i><h1>Upgrade</h1></div></a>
+    <a href="/logout"><div class="gr g6"><i class="material-icons">logout
+    </i><h1>Logout</h1></div></a>
+</div>
 </div>
 <script type="text/javascript">
 		window.onload = function(){onLinkedInLoad};
